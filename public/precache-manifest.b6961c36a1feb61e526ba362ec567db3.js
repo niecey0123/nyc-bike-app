4885f9aaa2fb2c8fa3fb8cd7c4ebe4bc579ee9b8
niecey0123 <s.crumpler11@gmail.com>
2019-10-27 15:52:38 -0400

    ready for first push to heroku

---
+++ public/precache-manifest.b6961c36a1feb61e526ba362ec567db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb788b85933220ad37cb49125ab3a46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a30fd30a9c0a8b88733",
+    "url": "/static/css/main.b88b9ab7.chunk.css"
+  },
+  {
+    "revision": "028652c85ef363672d2f",
+    "url": "/static/js/2.94938a5c.chunk.js"
+  },
+  {
+    "revision": "3a30fd30a9c0a8b88733",
+    "url": "/static/js/main.6a817b4a.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "7cc531a35d61cf6513e08ef258461364",
+    "url": "/static/media/landing.7cc531a3.jpg"
+  },
+  {
+    "revision": "96d6dbed65c1a092c9d6cdfb5c741ec2",
+    "url": "/static/media/logo.96d6dbed.png"
+  }
+]);
