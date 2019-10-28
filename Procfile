2b2868318a1e48fe8d30df2d602bdba8df69bd92
niecey0123 <s.crumpler11@gmail.com>
2019-10-28 10:51:26 -0400

    first Heroku deploy

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: cd client && npm start
-api: bundle exec rails s -p 4567
+web: bundle exec puma -t 5:5 -p ${PORT:-4567} -e ${RACK_ENV:-development}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :products
 
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
 end
