ae67113f9f8f291435c542c7930b5b2d4d0aa149
niecey0123 <s.crumpler11@gmail.com>
2019-10-30 07:35:12 -0400

    ..

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,6 +1,7 @@
 Datadog.configure do |c|
     c.use :rails, service_name: 'rails_products_api',
-    c.analytics_enabled = true
+    
   end
 
   
+# Datadog.configure { |c| c.analytics_enabled = true }
